9b321f7826fd74ee65a68ef0ac85d838def1dd2b
Joan <Joanpadolina@gmail.com>
2020-04-03 11:34:48 +0200

    styling

--- public/js/index.js
+++ public/js/index.js
@@ -4,40 +4,84 @@ const h2 = document.querySelector('.live-update')
 // h2.contentEditable = "true";
 const text = document.querySelector('input[type="text"]')
 
-text.addEventListener('input', () => {
-    h2.innerHTML = text.value
-})
-h2.addEventListener('input', () => {
-    text.value = h2.innerHTML
-    
-})
+if (document.addEventListener) {
+    text.addEventListener('input', () => {
+        h2.innerHTML = text.value
+    })
+} else if (document.attachEvent) {
+    text.attachEvent('oninput', () => {
+        h2.innerHTML = text.value
+    })
+}
+
+if (document.addEventListener) {
+    h2.addEventListener('input', () => {
+        text.value = h2.innerHTML
+
+    })
+} else if (document.attachEvent) {
+    h2.addEventListener('oninput', () => {
+        text.value = h2.innerHTML
+
+    })
+}
+
+
 
 
 // change color shirt
 const colorShirt = document.getElementsByName('colorshirt')
 const svg = document.querySelector('svg')
 
-for (const item of colorShirt) {
-    item.addEventListener('change', () => {
+if (document.addEventListener) {
+    for (const item of colorShirt) {
+        item.addEventListener('change', () => {
             svg.setAttribute('style', `fill:${item.value}`)
-    })
+        })
+    }
+} else if (document.attachEvent) {
+    for (const item of colorShirt) {
+        item.attachEvent('onchange', () => {
+            svg.setAttribute('style', `fill:${item.value}`)
+        })
+    }
 }
+
 
 //change color text
 const colorText = document.getElementsByName('colortext')
-for(const item of colorText){
-    item.addEventListener('change', () => {
-        h2.setAttribute('style', `color:${item.value}`)
-    })
+
+if (document.addEventListener) {
+    for (const item of colorText) {
+        item.addEventListener('change', () => {
+            h2.setAttribute('style', `color:${item.value}`)
+        })
+    }
+} else if (document.attachEvent) {
+    for (const item of colorText) {
+        item.attachEvent('onchange', () => {
+            h2.setAttribute('style', `color:${item.value}`)
+        })
+    }
 }
+
 
 //change icon 
 const iconSet = document.getElementsByName('hero')
 const iconLabel = document.querySelectorAll('input[name=hero]')
 console.log(iconLabel.label)
 const iconPlace = document.querySelector('.icon')
-for(const item of iconSet){
-    item.addEventListener('change', () => {
-        iconPlace.setAttribute('src', `/img/svg/icon/${item.value}.svg`)
-    })
+
+if (document.addEventListener) {
+    for (const item of iconSet) {
+        item.addEventListener('change', () => {
+            iconPlace.setAttribute('src', `/img/svg/icon/${item.value}.svg`)
+        })
+    }
+} else if (document.attachEvent) {
+    for (const item of iconSet) {
+        item.attachEvent('onchange', () => {
+            iconPlace.setAttribute('src', `/img/svg/icon/${item.value}.svg`)
+        })
+    }
 }
